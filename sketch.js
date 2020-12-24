632dffbef5b299c294173c9d8c9f20dca0056782
Nobody03 <70205350+JasviDoshi@users.noreply.github.com>
2020-12-24 16:46:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,105 @@
+var dog, happyDog, database, foodS, foodStock
+var dogImg, dogHappyImg;
+var milk, milkImg;
+
+
+function preload()
+{
+  dogImg = loadImage("images/dogImg.png");
+  dogHappyImg = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+  database = firebase.database();
+  createCanvas(500, 500);
+  
+  dog = createSprite(250,250,10,10);
+  dog.addImage(dogImg);
+  dog.scale = 0.15;
+
+  emo = createSprite(200,200,1,1);
+  
+  foodStock = database.ref('food');
+  foodStock.on("value",readStock);
+  foodStock.set(50);
+
+  for (var i = 5; i < 500; i=i+10) 
+{
+
+var dot = createSprite(i, 5, 3, 3);
+dot.shapeColor = "yellow";
+
+}
+for (var i = 5; i < 500; i=i+10) 
+{
+
+var dot1 = createSprite(i, 495, 3, 3);
+dot1.shapeColor = "yellow";
+
+}
+for (var i = 5; i < 500; i=i+10) 
+{
+
+var dot1 = createSprite(495,i, 3, 3);
+dot1.shapeColor = "yellow";
+
+}
+for (var i = 5; i < 500; i=i+10) 
+{
+
+var dot1 = createSprite(5,i, 3, 3);
+dot1.shapeColor = "yellow";
+
+}
+}
+
+
+function draw() {  
+  background(46,139,87)
+
+  if(foodS !== 0){
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogHappyImg);
+  }
+
+  if(keyWentUp(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(dogImg);
+  }
+}
+
+if(foodS == 0){
+  dog.addImage(dogImg);
+  foodS = 20;
+}
+
+
+
+  drawSprites();
+  textSize(17);
+  fill("yellow");
+  text("Feed your puppy- Padfoot",100,150);
+  fill("white");
+  text("Press your up arrow key to feed Padfoot",50,50);
+  fill("cyan");
+  text("Milk Bottles Remaining: "+foodS,170,440);
+}
+
+function readStock(data)
+{
+  foodS = data.val();
+}
+
+function writeStock(x){
+
+  if(x<=0){
+    x = 0;
+  }else{
+    x=x-1
+  }
+
+  database.ref('/').update({
+    food:x
+  })
+}
